5d525bf220844c024845cb0a240743209859c2e1
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2025-03-27 10:56:21 +0100

    Local notifications demo for Android

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'notification_form.dart';
+import 'notification_service.dart';
+import 'theme.dart';
+
+void main() async {
+  final notification = NotificationService();
+  await notification.initialize();
+  runApp(
+    Provider<NotificationService>.value(
+      value: notification,
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final title = 'Local Notifications Demo';
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<NotificationService>().setupCallbacks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: MyApp.navigatorKey,
+      title: title,
+      color: brandColor,
+      theme: theme,
+      darkTheme: theme,
+      home: Scaffold(
+        appBar: AppBar(title: Text(title), centerTitle: true),
+        body: const NotificationForm(),
+      ),
+    );
+  }
+}
